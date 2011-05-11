4d45db60f712483a9f45393cd0e645a3bca839d2
papricek <patrikjira@gmail.com>
2011-05-11 11:51:05 +0200

    Mainpage with sticky nav and footer at bottom

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,22 @@
-/* Author: 
+var Application = function() {
 
-*/
+    return {
+        initStickyNav: function() {
+            var sidebar = $("aside nav");
+            var top = sidebar.offset().top - parseFloat(sidebar.css("marginTop"));
 
+            $(window).scroll(function (e) {
+                var ypos = $(this).scrollTop();
+                if (ypos >= top) {
+                    sidebar.addClass("fixed");
+                }
+                else {
+                    sidebar.removeClass("fixed");
+                }
+            });
+        }
+    }
+}();
 
 
 
