f2765ed089875e521272a9c85da64e3ecb208d15
papricek <patrikjira@gmail.com>
2011-05-12 16:25:24 +0200

    Disqus improvements

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,7 @@
 var Application = function() {
 
+    var DISQUS_API_KEY = "zrPjvWYQajxfGedOasCvs052F6LKUxszCUSgcl5e5G3mpnl0fNlqUEhouTNDUv1l";
+
     return {
         initStickyNav: function() {
             var sidebar = $("aside nav");
@@ -14,28 +16,24 @@ var Application = function() {
                     sidebar.removeClass("fixed");
                 }
             });
+        },
+
+        initDisqusSlider: function(){
+            var disqus = $("#disqus_thread");
+            var button = $("#disqus-button");
+            var arrow = $("#disqus-button-arrow");
+            button.click(function(e){
+                e.preventDefault();
+                disqus.slideToggle("slow", function(){
+                    if(disqus.is(":visible")){
+                        arrow.html("&uarr;")
+                    }
+                    else {
+                        arrow.html("&darr;")
+                    }
+                });
+            })
+
         }
     }
 }();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
